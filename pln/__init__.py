9c2ca7188c4618a745dfc81c4ad1ab32e5107216
Taurus Olson <taurusolson@gmail.com>
2015-09-03 21:13:41 +0200

    Bump to 0.0.2

--- pln/__init__.py
+++ pln/__init__.py
@@ -2,7 +2,7 @@ from .core import Pipeline, Watch, shift
 
 
 __title__ = 'pln'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Taurus Olson'
 __license__ = 'MIT'
 __email__ = 'taurusolson@gmail.com'
